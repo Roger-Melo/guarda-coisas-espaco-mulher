a14c7e32191cb6e033795c780d44fa64bf479547
Roger-Melo <rogerw592@gmail.com>
2023-11-03 20:23:39 -0300

    ✅ Adição de items

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,55 @@
+import { useState } from "react"
+
 const ids = Array.from({ length: 20 }, () => crypto.randomUUID())
 
-const App = () => (
-  <>
-    <form className="add-form">
-      <h3>O que você precisa guardar?</h3>
-      <select>
-        {ids.map((id, index) => (
-          <option key={id} value={index + 1}>
-            {index + 1}
-          </option>
-        ))}
-      </select>
-      <input placeholder="Manda aqui" autoFocus />
-      <button className="add-btn">Adicionar</button>
-    </form>
+const App = () => {
+  const [items, setItems] = useState([])
 
-    <div className="list">
-      <ul>
-        <li>
-          <input type="checkbox" />
-          <span>6 halteres 10kg</span>
-          <button>❌</button>
-        </li>
-      </ul>
-    </div>
-  </>
-)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { selectQtd, inputAdd } = e.target.elements
+
+    setItems((prev) => [
+      ...prev,
+      {
+        id: crypto.randomUUID(),
+        quantity: +selectQtd.value,
+        name: inputAdd.value,
+        stored: false,
+      },
+    ])
+  }
+
+  return (
+    <>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h3>O que você precisa guardar?</h3>
+        <select name="selectQtd">
+          {ids.map((id, index) => (
+            <option key={id} value={index + 1}>
+              {index + 1}
+            </option>
+          ))}
+        </select>
+        <input name="inputAdd" placeholder="Manda aqui" autoFocus />
+        <button className="add-btn">Adicionar</button>
+      </form>
+
+      <div className="list">
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <input type="checkbox" />
+              <span>
+                {item.quantity} {item.name}
+              </span>
+              <button>❌</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
 
 export { App }
