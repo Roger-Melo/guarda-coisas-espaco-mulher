bbd6eeb43f88cbce715b1c4d8947de610853d727
Roger-Melo <rogerw592@gmail.com>
2023-11-03 20:11:23 -0300

    ✅ Marcação inicial form e lista

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,30 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const ids = Array.from({ length: 20 }, () => crypto.randomUUID())
+
+const App = () => (
+  <>
+    <form className="add-form">
+      <h3>O que você precisa guardar?</h3>
+      <select>
+        {ids.map((id, index) => (
+          <option key={id} value={index + 1}>
+            {index + 1}
+          </option>
+        ))}
+      </select>
+      <input placeholder="Manda aqui" autoFocus />
+      <button className="add-btn">Adicionar</button>
+    </form>
+
+    <div className="list">
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <span>6 halteres 10kg</span>
+          <button>❌</button>
+        </li>
+      </ul>
+    </div>
+  </>
+)
 
 export { App }
