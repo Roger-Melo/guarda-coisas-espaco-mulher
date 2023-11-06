7c79a8756553bb8ef479a1786133203fcdaad3c6
Roger-Melo <rogerw592@gmail.com>
2023-11-06 20:44:49 -0300

    ✅ Add estatísticas de itens guardados

--- src/app.jsx
+++ src/app.jsx
@@ -68,6 +68,21 @@ const Filters = ({ orderBy, onChangeOrder }) => (
   </div>
 )
 
+const Stats = ({ items }) => {
+  const storedItems = items.reduce((acc, item) => item.stored ? acc + 1 : acc, 0)
+  const storedPercentage = items.length === 0 ? 0 : ((storedItems / items.length) * 100).toFixed(0)
+  const singularPlural = items.length === 1 ? "item" : "itens"
+
+  return (
+    <footer className="stats">
+      <p>
+        {`Você tem ${items.length} ${singularPlural} na lista`}
+        {items.length > 0 && <span> e já guardou {storedItems} ({storedPercentage}%)</span>}
+      </p>
+    </footer>
+  )
+}
+
 const App = () => {
   const [items, setItems] = useState(initialItems)
   const [orderBy, setOrderBy] = useState("newest")
@@ -116,6 +131,7 @@ const App = () => {
         />
         <Filters orderBy={orderBy} onChangeOrder={handleChangeOrder} />
       </div>
+      <Stats items={items} />
     </>
   )
 }
